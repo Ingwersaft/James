39a2934803cac87559b910f636c9c16c7704c313
Marcel Kesselring <Ingwersaft@users.noreply.github.com>
2018-01-25 16:24:09 +0100

    finished rough prototype of new communication design

--- src/main/kotlin/com/mkring/james/prototype/ChannelBasedJamesPrototype.kt
+++ src/main/kotlin/com/mkring/james/prototype/ChannelBasedJamesPrototype.kt
@@ -1,13 +1,9 @@
 package com.mkring.james.prototype
 
 import com.mkring.james.chatbackend.UniqueChatTarget
-import com.mkring.james.mapping.MappingPattern
+import com.mkring.james.chatbackend.log
+import kotlinx.coroutines.experimental.*
 import kotlinx.coroutines.experimental.channels.Channel
-import kotlinx.coroutines.experimental.channels.ReceiveChannel
-import kotlinx.coroutines.experimental.channels.SendChannel
-import kotlinx.coroutines.experimental.delay
-import kotlinx.coroutines.experimental.launch
-import kotlinx.coroutines.experimental.runBlocking
 
 data class IncomingPayload(
     val target: UniqueChatTarget,
@@ -21,20 +17,47 @@ data class OutgoingPayload(
     val options: Map<String, String> = emptyMap()
 )
 
-abstract class ChatBackendV3(
-    val backendToJamesChannel: SendChannel<IncomingPayload>,
-    val fromJamesToBackendChannel: ReceiveChannel<OutgoingPayload>
-) {
-    //abstract fun getJamesToBackendChannel(): ReceiveChannel<OutgoingPayload>
-    abstract fun start()
+abstract class ChatBackendV3 {
+    val backendToJamesChannel: Channel<IncomingPayload> = Channel(2)
+    val fromJamesToBackendChannel: Channel<OutgoingPayload> = Channel(2)
+
+    abstract suspend fun start()
 }
 
-class MainClassChatBackend(
-    backendToJamesChannel: SendChannel<IncomingPayload>,
-    fromJamesToBackendChannel: ReceiveChannel<OutgoingPayload>
-) : ChatBackendV3(backendToJamesChannel, fromJamesToBackendChannel) {
+class ChatV2(
+    val mappings: MutableMap<String, MappingV2.() -> Unit>,
+    val chatBackendV3: ChatBackendV3
+) {
+    private val runningJobs = mutableMapOf<UniqueChatTarget, Job>()
+    suspend fun start() {
+        chatBackendV3.start()
+        println("ChatV2 chat started, now iterating chatBackend")
+        launch {
+            while (true) {
+                chatBackendV3.backendToJamesChannel.receive().let { (uniqueChatTarget, username, text) ->
+                    println("chatBackendV3.backendToJamesChannel received $text from $username - $uniqueChatTarget")
+                    println("mappings=$mappings")
+                    mappings.map { it.key }.joinToString(";").let { println("chatLogicMappings keys =$it") }
 
-    override fun start() {
+                    mappings.entries.firstOrNull()?.let { entry ->
+                        log.info("going to handle ${entry.key}")
+                        val job = launch {
+                            MappingV2(text, uniqueChatTarget, username, chatBackendV3.fromJamesToBackendChannel).apply {
+                                entry.value.invoke(this)
+                            }
+                        }
+                        log.info("launch done")
+                        runningJobs.put(uniqueChatTarget, job)
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+class MainClassChatBackend : ChatBackendV3() {
+    override suspend fun start() {
         println("start called")
         launch {
             while (true) {
@@ -48,48 +71,42 @@ class MainClassChatBackend(
     }
 
     suspend fun onMessage(text: String) {
+        println("onMessage called")
         backendToJamesChannel.send(IncomingPayload("<id>", "<user>", text))
+        println("onMessage done")
     }
+
 }
 
-class ChatV2(
-    val mappings: MutableMap<MappingPattern, MappingV2.() -> Unit>,
-    val chatBackendV3: ChatBackendV3
-    // TODO
+class MappingV2(
+    private val commandText: String,
+    private val uniqueChatTarget: UniqueChatTarget,
+    val username: String?,
+    private val fromJamesToBackendChannel: Channel<OutgoingPayload>
 ) {
-    fun todo() {
-        chatBackendV3
-    }
-}
-
-class MappingV2(private val incomingPayload: IncomingPayload, private val chat: ChatV2) {
 
     fun send(text: String, options: Map<String, String> = emptyMap()) {
-        TODO()
+        val deferred = async {
+            fromJamesToBackendChannel.send(OutgoingPayload(uniqueChatTarget, text))
+        }.awaitBlocking()
     }
+}
+
+private fun <T> Deferred<T>.awaitBlocking(): T = runBlocking {
+    await()
 }
 
 fun main(args: Array<String>) = runBlocking {
-
-    val outgoing: Channel<OutgoingPayload> = Channel()
-    val incoming: Channel<IncomingPayload> = Channel()
-
-    launch {
-        while (true) {
-            incoming.receive().let {
-                // fake found mapping
-
-                println("received from chat: $it")
-                println("going to answer")
-                outgoing.send(OutgoingPayload(it.target, "sdsd"))
-            }
-            delay(10)
-        }
+    val chat = MainClassChatBackend()
+    val mapping: MappingV2.() -> Unit = {
+        send("world")
+    }
+    ChatV2(mutableMapOf("/hallo" to mapping), chat).apply {
+        start()
     }
 
-    val chat = MainClassChatBackend(incoming, outgoing).apply { start() }
+    println("wait then onMessage^")
     delay(100)
-    chat.onMessage("/hallo welt")
+    chat.onMessage("/hallo")
     delay(1000)
-    chat.onMessage("sdkjdk")
 }
