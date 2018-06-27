98dee3d94a11d05c5140513cb2ced42f5f0ed625
Ingwersaft <Ingwersaft@users.noreply.github.com>
2018-06-27 20:21:20 +0200

    small test cleanup

--- src/test/kotlin/com/mkring/james/mapping/MappingTest.kt
+++ src/test/kotlin/com/mkring/james/mapping/MappingTest.kt
@@ -5,12 +5,13 @@ import com.mkring.james.chatbackend.ChatBackend
 import com.mkring.james.chatbackend.IncomingPayload
 import com.mkring.james.chatbackend.OutgoingPayload
 import com.mkring.james.chatbackend.UniqueChatTarget
-import com.mkring.james.fireAndForgetLoop
 import com.mkring.james.james
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertTrue
 import kotlinx.coroutines.experimental.CompletableDeferred
+import kotlinx.coroutines.experimental.channels.consumeEach
 import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
 import kotlinx.coroutines.experimental.runBlocking
 import org.junit.Test
 import org.slf4j.LoggerFactory
@@ -205,22 +206,21 @@ class MappingTest {
 }
 
 class TestBackend : ChatBackend() {
-
     var outgoing: MutableList<OutgoingPayload> = mutableListOf()
+
     suspend fun send(text: String, id: UniqueChatTarget = "don't care", user: String = "someone") {
         backendToJamesChannel.send(IncomingPayload(id, user, text))
     }
 
     override suspend fun start() {
-        fireAndForgetLoop("james-to-backend") {
-            fromJamesToBackendChannel.receive().let {
+        launch {
+            fromJamesToBackendChannel.consumeEach {
                 outgoing.add(it)
                 println("received on james-to-backend: $it")
+                delay(10)
             }
-            delay(10)
         }
     }
-
 }
 
 suspend fun waitForAnswers(testBackend: TestBackend, answerAmount: Int, times: Int = 20, duration: Long = 10) {
