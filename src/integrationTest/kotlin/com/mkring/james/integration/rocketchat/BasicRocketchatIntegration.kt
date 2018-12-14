c589334ee0df6da31f9a18a06de1d9e59b66f525
Ingwersaft <Ingwersaft@users.noreply.github.com>
2018-12-14 12:29:19 +0100

    added james to testcase

--- src/integrationTest/kotlin/com/mkring/james/integration/rocketchat/BasicRocketchatIntegration.kt
+++ src/integrationTest/kotlin/com/mkring/james/integration/rocketchat/BasicRocketchatIntegration.kt
@@ -1,5 +1,6 @@
 package com.mkring.james.integration.rocketchat
 
+import com.mkring.james.james
 import junit.framework.TestCase.assertTrue
 import org.junit.Test
 import java.net.URL
@@ -9,13 +10,22 @@ class BasicRocketchatIntegration {
     @Test
     fun testConnectivity() {
         println("### testConnectivity ###")
-        try {
-            val readText = URL("http://localhost:3000").readText()
-            assertTrue(readText.isNotEmpty())
-        } catch (e: Exception) {
-            println("e: ${e::class.simpleName}# ${e.message}")
-            e.printStackTrace()
-            throw e;
+        val readText = URL("http://localhost:3000").readText()
+        assertTrue(readText.isNotEmpty())
+
+        // setup james
+        james {
+            rocketchat {
+                ignoreInvalidCa = true
+                sslVerifyHostname = false
+                username = "bot"
+                password = "1234"
+                websocketTarget = "wss://localhost/websocket"
+            }
+            map("ping", "") {
+                send("pong")
+            }
         }
+        println("james started!")
     }
 }
